dc21f47cedebc52dad2d4d764029744a78786727
philroe <philr@compuserve.com>
2013-11-17 21:44:45 -0500

    major changes. couldn't get themeRoller to work.

---
+++ delete.js
@@ -0,0 +1,45 @@
+// Java Document
+<script language="javascript" type="text/javascript">
+function multiply(){
+a=Number(document.calculator.number1.value);
+b=Number(document.calculator.number2.value);
+c=a*b;
+document.calculator.total.value=c;
+}
+</script>
+
+<script language="javascript" type="text/javascript">
+function addition(){
+a=Number(document.calculator.number1.value);
+b=Number(document.calculator.number2.value);
+c=a+b;
+document.calculator.total.value=c;
+}
+</script>
+
+<script language="javascript" type="text/javascript">
+function subtraction(){
+a=Number(document.calculator.number1.value);
+b=Number(document.calculator.number2.value);
+c=a-b;
+document.calculator.total.value=c;
+}
+</script>
+
+<script language="javascript" type="text/javascript">
+function division(){
+a=Number(document.calculator.number1.value);
+b=Number(document.calculator.number2.value);
+c=a/b;
+document.calculator.total.value=c;
+}
+</script>
+
+<script language="javascript" type="text/javascript">
+function modulus(){
+a=Number(document.calculator.number1.value);
+b=Number(document.calculator.number2.value);
+c=a%b;
+document.calculator.total.value=c;
+}
+</script>
